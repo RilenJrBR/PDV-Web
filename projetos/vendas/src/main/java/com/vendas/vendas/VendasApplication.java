b63deaa8cfef7f91d0f0a57355657908bb80698b
Rilen Freitas <81658722+RilenJrBR@users.noreply.github.com>
2022-02-24 00:41:25 -0300

    implementando configurações

--- projetos/vendas/src/main/java/com/vendas/vendas/VendasApplication.java
+++ projetos/vendas/src/main/java/com/vendas/vendas/VendasApplication.java
@@ -2,8 +2,10 @@ package com.vendas.vendas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class VendasApplication {
 
 	public static void main(String[] args) {
