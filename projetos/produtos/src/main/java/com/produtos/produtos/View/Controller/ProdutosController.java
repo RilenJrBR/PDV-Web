6bde828e2651bfe0094fa28b3211dbbff0430159
Rilen Freitas <81658722+RilenJrBR@users.noreply.github.com>
2022-02-25 00:00:11 -0300

    atualização

--- projetos/produtos/src/main/java/com/produtos/produtos/View/Controller/ProdutosController.java
+++ projetos/produtos/src/main/java/com/produtos/produtos/View/Controller/ProdutosController.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping("api/produto")
 public class ProdutosController {
     @Autowired
     private ProdutosService servico;
